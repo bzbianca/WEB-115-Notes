efdcb131b63c221a5ff123e610901b40160c861a
Bianca Baccay <bzbaccay@my.waketech.edu>
2025-01-17 13:49:52 -0500

    1/17/2024 Notes

--- index.js
+++ index.js
@@ -62,3 +62,77 @@ document.getElementById("myButton").addEventListener("click", function(){
 
 console.log(document.getElementById("paragraph2").innerHTML) // Outputs: "<b>text</b>"
 console.log(document.getElementById("paragraph2").textContent) // Outputs: "text"
+
+// Variables
+// let x; // Declaration, can reference it like python 
+// console.log(x)
+// x = 2;
+// console.log(x)
+// const y = 10; // Initialization
+// let z = 3; 
+
+// x = "hello" // string
+// console.log(x)
+
+// z = true; // boolean
+// console.log(z)
+
+// let studentName = window.prompt("What is your name")
+
+// console.log(studentName);
+
+// window.alert(`Hello, your name is ${studentName}`) // backticks necessary
+
+// let online = false;
+// console.log(online)
+// console.log(`Online Status: ${online}`)
+// let enrolled = true;
+// console.log(enrolled)
+// console.log(`Enrolled: ${enrolled}`)
+
+// ARITHMETIC OPERATORS = Operands
+// +, -, *, l, **, %, ++, --
+
+let students = 24;
+console.log(students)
+students = students + 1;
+console.log(students)
+students += 1; // Augmented Assignment Operators
+// students -= 1;
+// students *= 1;
+// students /= 1;
+// students **= 1;
+// students %= 1;
+students++;
+console.log(students)
+students--;
+console.log(students)
+
+// Order of Operations
+// 1. Parentheses
+// 2. Exponent
+// 3. Multiplication & Division
+// 4. Addition & Subtraction
+
+let className = "WEB-115";
+console.log(className);
+console.log(typeof className);
+console.log(className, typeof className)
+myNumber = 5;
+console.log(typeof myNumber)
+
+let myAge = window.prompt("How old are you?")
+console.log(typeof myAge)
+myAge = Number(myAge) // Convert to number
+console.log(typeof myAge)
+
+let x = "pizza";
+let y = "pizza";
+let z = "";
+
+x = Number(x);
+console.log(x);
+y = String(y);
+console.log(typeof y)
+z = Boolean(z); // checks if the string has stuff in it, false = empty, true = has string
+console.log(z)
