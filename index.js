f181385ebb2f92f2ca351c7e1ed0547f347c87f5
Bianca Baccay <bzbaccay@my.waketech.edu>
2025-02-10 13:17:07 -0500

    2/10/2025

--- index.js
+++ index.js
@@ -416,3 +416,118 @@ for(let i = 1; i <= 20; i++){
 * continue; will skip iteration
 * break; will exit the loop
 */
+
+// 1D Array = a variable like a structure that can hold more than 1 value.
+// 1D Array = a list from Python
+
+let fruits = ["apple", "orange", "banana"];
+console.log(fruits[0]);
+fruits[3] = "coconut"; // Add to new [3] of the array
+console.log(fruits[3]);
+
+fruits.push("coconut"); // Add element to end of array
+fruits.pop(); // Remove last element.
+fruits.unshift("mango") // Add element to beginning of array.
+fruits.shift(); // remove element from beginning.
+
+let numOfFruits = fruits.length;
+console.log(numOfFruits);
+console.log(fruits.length);
+
+let index = fruits.indexOf("apple"); // Search for index of matching.
+console.log(index);
+// If .indexOf doesn't find a match, then returns -1.
+// Use in an if statement to do something if no match (if .indexOf() = -1...)
+
+// Iterate through an array.
+
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
+
+// Iterate through array backwards.
+for(let i = fruits.length - 1; i >= 0; i--){
+    console.log(fruits[i]);
+}
+
+// // Enhanced for loop
+
+for(let fruit of fruits){
+    console.log(fruit);
+}
+
+// Sort method
+fruits.sort();
+console.log(fruits);
+// Reverse sort.
+fruits.reverse();
+console.log(fruits);
+
+// 2D Arrays = a multi-dimensional array that stores a matrix of data in rows and columns.
+// Useful for games, spreadsheets, image representations
+
+//  [X, O, X]
+//  [O, X, O]
+//  [X, O, O]
+
+const matrix = [[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]];
+console.log(matrix)
+
+for (let row of matrix){
+    // console.log(row) // 1st
+    const rowString = row.join(' '); 
+    // .join adds something between each array element
+    console.log(rowString)
+}
+
+// matrix[row][column]
+matrix[0][0] = 'X';
+matrix[0][1] = 'O';
+matrix[0][2] = 'X';
+matrix[1][0] = 'O';
+matrix[1][1] = 'X';
+matrix[1][2] = 'O';
+matrix[2][0] = 'X';
+matrix[2][1] = 'X';
+matrix[2][2] = 'X';
+
+
+const numPad = [[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9],
+                ['*', 0, '#']];
+console.log(numPad)
+
+// Objects = like a Dictonary in Python
+// NOT the same as "Objects" from the term Object-Oriented Programming!!
+
+let person = {
+    // Name/Value pairs
+    firstName : "John",
+    lastName : "Smith",
+    age : 46,
+    email: "johnsmith@email.com"
+}
+
+console.log(person)
+// Dot notation
+console.log(person.firstName)
+
+// Bracket Notation
+
+// console.log(person[age]) // Will not work.
+console.log(person["age"]) // Assuming a variable.
+console.log(person["age"] + 10)
+
+// Looping through Object - very particular
+for (i in person){
+    // console.log[i]; // Will not work
+    // console.log(person.i) // Also will not work.
+    console.log(person[i]) // THIS WORKS!!!
+}
+
+for (i in person) {
+    console.log(i + ": " + person[i]);
+}
