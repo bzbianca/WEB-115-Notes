fa989b9907756646ad67dd6358bfb516bcf015fd
Bianca Baccay <bzbaccay@my.waketech.edu>
2025-04-22 13:40:56 -0400

    4/22/2025

--- M10andOnwards.js
+++ M10andOnwards.js
@@ -313,3 +313,234 @@ Scope is a place to store variables
 
 //     return {increase, decreaseScore, getScore}
 // }
+
+
+// Method Chaining = Calling one method after another in one continous line of code
+
+// let username = window.prompt("Enter your username: ")
+
+// // ---- NO METHOD CHAINING -----
+
+// username = username.trim();
+// let letter = username.charAt(0);
+// letter = letter.toUpperCase();
+
+// let extraChars = username.slice(1);
+// extraChars = extraChars.toLowerCase();
+// username = letter + extraChars;
+
+// console.log(username);
+
+// // ----- METHOD CHAINING -----
+
+// username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+
+// console.log(username)
+
+
+
+// callback = a function that is passed as an argument to another function
+//      used to handle asynchronous operations
+
+
+// // EXAMPLE 1:
+// hello(goodbye); // put function name in paratheses to ensure hello is executed first and the one inside is executed next
+
+
+// function hello(callback){ // callback in parameter to allow function to be in the calling function
+//     console.log("Hello!");
+// }
+
+
+// function leave(){
+//     console.log("Leave!")
+// }
+
+// function goodbye(){
+//     console.log("Goodbye!");
+// }
+
+// // EXAMPLE 2:
+
+// sum(displayConsole, 1, 2)
+
+// function sum(callback, x, y){
+//     let result = x + y;
+//     callback(result);
+// }
+
+// function displayConsole(result){
+//     console.log(result)
+// }
+
+// // pretend you have an html element called H1
+// function displayPage(result){
+//     document.getElementById("myH1").textContent = result;
+// }
+
+
+// synchronous = executes line by line consecutively in a sequential manner
+//      Code that waits for an operation to complete
+
+// asynchronous = Allows multiple operations to be performed concurrently without waiting
+//      Doesn't block the execution flow and allows the program to continue
+//      (I/O operations, network requests, fetching data)
+//      Handled with: Callbacks, Promises, Async/Await
+
+function func1(){
+    setTimeout(() => {console.log("Task 1");
+                        callback()}, 3000);
+}
+
+function func2(){
+    console.log("Task 2")
+    console.log("Task 3")
+    console.log("Task 4")
+}
+
+func1(func2)
+
+
+// Callback Hell = Situation in JavaScript where callbacks are nested within other callbacks to the degress where the code eis difficult to read.
+//      Old pattern to handle asynchronous functions
+//      Use Promises + async/await to avoid Callback Hell
+// function task1(){
+//     setTimeout(() => {
+//         console.log("Task 1 complete"); 
+//         callback()
+//     }, 2000)
+// }
+// function task2(){
+//     setTimeout(() => {
+//         console.log("Task 2 complete"); 
+//         callback()
+//     }, 1000)
+// }
+// function task3(){
+//     setTimeout(() => {
+//         console.log("Task 3 complete"); 
+//         callback()
+//     }, 3000)
+// }
+// function task4(){
+//     setTimeout(() => {
+//         console.log("Task 4 complete"); 
+//         callback()
+//     }, 1500)
+// }
+
+// task1(() => {
+//     task2(() => {
+//         task3(() => {
+//             task4(() => console.log("All tasks are completed."))
+//         })
+//     })
+// })
+
+
+// Promise = An Object that manages asynchronous operations.
+//      Wrap a Promise Object around {asynchronous code}
+//      "I promise to return a value"
+//      PENDING -> RESOLVED OR REJECTED
+//      new Promise((resolve, reject) => {asynchronous code})
+
+// DO THESE CHORES IN ORDER
+
+// 1. WALK THE DOG
+// 2. CLEAN THE KITCHEN
+// 3. TAKE OUT THE TRASH
+
+function walkDog(){
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const dogwalked = true;
+
+            if(dogwalked){
+                resolve("You walk the dog");
+            } else {
+                reject("You didn't walk the dog")
+            }
+        }, 1500);
+    });
+}
+
+function cleanKitchen(){
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const kitchenCleaned = true;
+
+            if(kitchenCleaned) {
+                resolve("You clean the kitchen");
+            } else {
+                reject("You didn't clean the kitchen")
+            }
+        }, 2500);
+    })
+}
+
+function takeOutTrash(){
+
+    
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const trashTaken = true;
+
+            if(trashTaken) {
+                resolve("You take out the trash");
+            } else {
+                reject("You didn't take out the trash")
+            }
+        }, 500);
+    })
+}
+
+// walkDog().then(value => {console.log(value); return cleanKitchen()})
+//          .then(value => {console.log(value); return takeOutTrash()})
+//          .then(value => {console.log(value); console.log("You finished all the chores")})
+//          .catch(error => console.error(error));
+
+// Async/Await = Async = makes a function return a promise
+//               Await = maskes an async function wait for a promise
+
+//               Allows you write asyncronous code in a synchronous manner
+//               Async doesn't have resolve or reject parameters
+//               Everything after Await is placed in an event queue
+
+async function doChores(){ // await depends on async
+    
+    try{
+        const walkDogResult = await walkDog();
+        console.log(walkDogResult);
+
+        const cleanKitchenResult = await cleanKitchen();
+        console.log(cleanKitchenResult);
+
+        const takeOutTrashResult = await takeOutTrash();
+        console.log(takeOutTrashResult);
+
+        console.log("You finished all the chores!");
+    }
+    catch(error){
+        console.error(error)
+    }
+}
+
+// // Marcus is a bully so i made a promise
+// function marcusIs(){
+
+//     return new Promise((resolve, reject) => {
+//         const marcusBullies = true;
+
+//         if (marcusBullies) {
+//             resolve("Anonymous Reporting System.")
+//         } else{
+//             reject("Marcus is.")
+//         }
+//     })
+// }
+// marcusIs().then(value => {console.log(value)}).catch(error => console.error(error))
